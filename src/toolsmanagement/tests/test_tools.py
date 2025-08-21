0dabb9f0090757597591dda1e4e4881c9fb35ccd
rlossec <raphaelossec@gmail.com>
2025-08-21 21:25:47 +0200

    Test : Basic Test for - Create Tool -

--- src/toolsmanagement/tests/test_tools.py
+++ src/toolsmanagement/tests/test_tools.py
@@ -34,6 +34,14 @@ class ToolTestCase(APITestCase):
                                           vendor="Test Vendor 2",
                                           category=self.category_2,
                                           base_monthly_cost=200)
+        self.body = {
+            "name": "Test Create UpdateTool 1",
+            "website_url": "https://test-create-update-tool-1.com",
+            "description": "Test Create Update Description 1",
+            "owner_department": "EN",
+            "vendor": "Test Create Update Vendor 1",
+            "category": self.category_1.id,
+            "base_monthly_cost": 100}
 
     def test_list_tools(self):
         url = reverse("tool-list")
@@ -43,3 +51,14 @@ class ToolTestCase(APITestCase):
         self.assertEqual(response.data[0]["name"], "Test Tool 1")
         self.assertEqual(response.data[1]["name"], "Test Tool 2")
 
+    def test_create_tool(self):
+        url = reverse("tool-list")
+        response = self.client.post(url, self.body)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data["name"], "Test Create UpdateTool 1")
+        self.assertEqual(response.data["website_url"], "https://test-create-update-tool-1.com")
+        self.assertEqual(response.data["description"], "Test Create Update Description 1")
+        self.assertEqual(response.data["owner_department"], "EN")
+        self.assertEqual(response.data["vendor"], "Test Create Update Vendor 1")
+        self.assertEqual(response.data["category"], self.category_1.id)
+        self.assertEqual(response.data["base_monthly_cost"], 100.00)
