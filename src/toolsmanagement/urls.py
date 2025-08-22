b8cfab3ea6d147bf359095d6823ddfd665173622
rlossec <raphaelossec@gmail.com>
2025-08-22 14:38:09 +0200

    Fix imports

--- src/toolsmanagement/urls.py
+++ src/toolsmanagement/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from toolsmanagement.views.tool import ToolViewSet
+
+from .views import ToolViewSet
 
 router = DefaultRouter()
 router.register(r'tools', ToolViewSet, basename='tool')
