ce451d9d559a9b508c7182bc922d4f7f7d2353ea
rlossec <raphaelossec@gmail.com>
2025-08-22 08:55:31 +0200

    Add postgres env variables

--- src/config/settings.py
+++ src/config/settings.py
@@ -68,11 +68,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv("POSTGRES_ENGINE", "django.db.backends.postgresql"),
+        'NAME': os.getenv("POSTGRES_DB", "testdb"),
+        'USER': os.getenv("POSTGRES_USER", "testuser"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD", "testpass"),
+        'HOST': os.getenv("POSTGRES_HOST", "localhost"),
+        'PORT': os.getenv("POSTGRES_PORT", "5432"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
